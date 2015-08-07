53010f4797c1d64af542d7eaf010cf1f5221ffd9
Mike Farr <mikefarr@mac.com>
2015-08-06 20:47:44 -0700

    searching, adding, deleting working.  Lot's of annoying CSS and it's looking pretty good

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,15 +2,14 @@ class SongsController < ApplicationController
 
 # before_filter :authorize
 
-  # GET /songs
+# GET /songs
   def index
-     @songs = @songs ||= Song.all
+    @songs = @songs ||= Song.all
     # @songs = Song.all
   end
 
   def show
-    song_number = params[:id]
-    @song = Song.where(song_number: song_number).first
+    @song = current_song
   end
 
 #POST /songs/search
@@ -32,7 +31,7 @@ class SongsController < ApplicationController
         if options_string.length != 0
           options_string << " and "
         end
-        options_string << key.to_s + " like ?"
+        options_string << key.to_s + " ilike ?"
         options_params << '%' + value + '%'
       end
     end
@@ -43,46 +42,71 @@ class SongsController < ApplicationController
     render "songs/index"
   end
 
-  # GET /songs/1
-  # GET /songs.json
+# GET /songs/1
+# GET /songs.json
 
-  # GET /songs/new
-  # assumes views/songs/new.html.erb
+# GET /songs/new
+# assumes views/songs/new.html.erb
   def new
     @song = Song.new
+    @song_number = Song.last.song_number + 1
+
   end
 
   def edit
-
+    @song = current_song
+    @song_number = @song.song_number
   end
 
   def create
+
     @render_club_photo = false
-    @song = Song.new(song_params)
 
     respond_to do |format|
-      if @song.save
+      if @song = Song.create(song_params)
         format.html { redirect_to @song, notice: 'Song was successfully created.' }
-        format.json { render json: @song}# render(json: {})
+        format.json { render json: @song } # render(json: {})
       else
         format.html { render :new }
         format.json { render json: @song.errors, status: :unprocessable_entity }
       end
+    end
   end
+
   def update
+    @song = current_song
     @render_club_photo = false
+    respond_to do |format|
+      if @song.update(song_params)
+        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.json { render json: @song } # render(json: {})
+      else
+        format.html { render :new }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
 
   end
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_song
-      @song = Song.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def song_params
-      params.require(:song).permit(:title, :voicing, :composer, :arranger_one, :arranger_two, :category, :larger_work)
+  def destroy
+    @song = current_song
+    @song.destroy
+    respond_to do |format|
+      format.html { redirect_to songs_path, notice: 'Song was deleted.' }
+      format.json { render json: 'Song was deleted.' } # render(json: {})
     end
+  end
+
+  private
+# Use callbacks to share common setup or constraints between actions.
+  def current_song
+    Song.find(params[:id])
+  end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+  def song_params
+    params.require(:song).permit(:title, :voicing, :composer, :arranger_one, :arranger_two, :category, :larger_work,
+                                 :song_number)
   end
 
 end
